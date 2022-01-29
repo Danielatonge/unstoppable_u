80f134834b2cab01efa1a572f6e7566ddaf19eb9
Daniel Atonge <elamboatonge@gmail.com>
2022-01-29 14:42:23 +0300

    adds mock data

---
+++ src/mock/index.ts
@@ -0,0 +1,72 @@
+export const POSTS = [
+  {
+    id: "1",
+    user: {
+      id: "u1",
+      username: "johnpaul",
+      name: "John Paul",
+      image: "https://robohash.org/johnpaul",
+    },
+    createdAt: "2020-08-27T12:00:00.000Z",
+    content:
+      "John Paul is the High Performance Executive Business Success Coach known as the for creating billions of dollars w/ his mentees and #Business.",
+    image: "https://picsum.photos/350/300",
+    commentCount: 12,
+    likeCount: 54,
+  },
+  {
+    id: "2",
+    user: {
+      id: "u2",
+      username: "willsonMath",
+      name: "Willson Mathew",
+      image: "https://robohash.org/willsonMath",
+    },
+    createdAt: "2020-08-27T12:00:00.000Z",
+    content:
+      "We got is the High Performance Executive Business Success Coach known as the for creating billions of dollars w/ his mentees and #Business.",
+    image: "https://picsum.photos/350/300",
+    commentCount: 12,
+    likeCount: 54,
+  },
+      {
+        id: '3',
+        user: {
+            id: 'u3',
+            username: 'johnpaul',
+            name: 'John Paul',
+            image: 'https://robohash.org/johnpaul'
+        },
+        createdAt: '2020-08-27T12:00:00.000Z',
+        content: 'John Paul is the High Performance Executive Business Success Coach known as the for creating billions of dollars w/ his mentees and #Business.',
+        commentCount: 12,
+        likeCount: 54
+    },
+    {
+        id: '4',
+        user: {
+            id: 'u4',
+            username: 'johnpaul',
+            name: 'John Paul',
+            image: 'https://robohash.org/johnpaul'
+        },
+        createdAt: '2020-08-27T12:00:00.000Z',
+        content: 'John Paul is the High Performance Executive Business Success Coach known as the for creating billions of dollars w/ his mentees and #Business.',
+        commentCount: 12,
+        likeCount: 54
+    },
+    {
+        id: '5',
+        user: {
+            id: 'u5',
+            username: 'chrisTom',
+            name: 'Chris Paul',
+            image: 'https://robohash.org/chrisTom'
+        },
+        createdAt: '2020-08-27T12:00:00.000Z',
+        content: 'John Paul is the High Performance Executive Business Success Coach known as the for creating billions of dollars w/ his mentees and #Business.',
+        image: 'https://picsum.photos/350/300',
+        commentCount: 12,
+        likeCount: 54
+    },
+];
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,15 +1,39 @@
-import { Text, View } from "react-native";
-import React from "react";
+import { FlatList, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components/native";
+import { POSTS } from "../mock";
 
 const Container = styled.View`
   background-color: #ddd;
 `;
 
 export const Home = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchPosts = async () => {
+    setLoading(true);
+    try {
+      setTimeout(() => setPosts(POSTS), 2000);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
   return (
     <Container>
-      <Text> What is going on</Text>
+      {/* <FlatList
+        data={posts}
+        renderItem={({ item }) => <Text>{JSON.stringify(item)}</Text> }
+        keyExtractor={(item) => item.id}
+        refreshing={loading}
+        onRefresh={fetchPosts}
+      /> */}
     </Container>
   );
 };
